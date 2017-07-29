a06c84b4a1286b2108b98168267a9a26c292dca9
Clinton Jenkins <cjenkins@mersive.com>
2017-07-28 21:15:31 -0600

    Working version of reading a file to completion.  Need to hook in notify library now to continually read from BufReader as file changes.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,26 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 
-fn tail_file(file_path: String) -> io::Result<&BufReader<std::fs::File>> {
+fn tail_file(file_path: String) -> io::Result<BufReader<std::fs::File>> {
     let f = File::open(file_path)?;
-    let reader = tail_reader(&mut BufReader::new(f));
-    for line in reader.lines() {
-        let _ = io::stdout().write(line.unwrap().as_bytes());
+    let mut reader = BufReader::new(f);
+    let mut buf = String::new();
+    let mut line = reader.read_line(&mut buf);
+
+    loop {
+        match line {
+            Ok(size) => {
+                if size > 0 {
+                    let _ = io::stdout().write(buf.as_bytes());
+                    buf = String::new();
+                    line = reader.read_line(&mut buf);
+                }
+                else {
+                    break;
+                }
+            },
+            Err(_) => break,
+        }
     }
 
     return Ok(reader)
