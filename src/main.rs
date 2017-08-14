b0e5d8ef7a0854f660ee929d439296826ea0cf32
Clinton Jenkins <cjenkins@mersive.com>
2017-08-13 21:29:21 -0600

    Added websocket connection as well as default args to current webserver.  Probably need to add a retry loop on loss of websocket connection.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::path::{Path, PathBuf};
 
 use notify::{Watcher, RecommendedWatcher, RecursiveMode, DebouncedEvent};
 use std::sync::mpsc::{Receiver, Sender, channel};
-use std::thread;
+use std::thread::{JoinHandle, spawn};
 use std::time::Duration;
 
 use crc::{crc32};
@@ -76,74 +76,104 @@ fn verify_checksum(checksum: String, data: String) -> Result<String, ()> {
     }
 }
 
-fn main() {
-    let mut args = env::args();
-    let _ = args.next();
-    let data_file_path = "/sdcard/Android/data/com.mersive.solstice.server/files/Logs/server.json";
-    //args.next().unwrap();
-    let websocket_address = "ws://192.168.2.19:8443/produce";
-    //args.next().unwrap();
-    
-    let path = Path::new(&data_file_path);
-    if path.exists() && path.is_file() {
-        let (mut reader, line_tx, line_rx) = tail_file(path.to_path_buf()).unwrap();
-        
-        let parent = path.parent().unwrap();
-        let (watcher, rx) = start_watching(parent.to_path_buf()).unwrap();
+fn start_websocket_thread(line_rx: Receiver<String>, websocket_address: String) -> JoinHandle<()> {
+    let mut websocket_builder = ClientBuilder::new(&websocket_address)
+        .expect("Failed to create websocket.");
 
-        let mut websocket_client = ClientBuilder::new(&websocket_address)
-            .unwrap()
-            .connect_insecure()
-            .unwrap();
-        
-        let receiver_thread = thread::spawn( move || {
-            loop {
-                let crc_string = line_rx.recv().unwrap();
-                let data = line_rx.recv().unwrap();
-                
-                match verify_checksum(crc_string, data) {
-                    Ok(s) => websocket_client.send_message(&OwnedMessage::Text(s)).unwrap(),
-                    Err(_) => println!("Failed checksum"),
-                }
-            }
-        });
-        
+    let mut websocket_client = websocket_builder.connect_insecure()
+        .expect("Failed to connect websocket");
+    
+    let receiver_handle = spawn( move || {
         loop {
-            let event = rx.recv().unwrap();
+            let crc_string = line_rx.recv().expect("Failed to receive CRC string.");
+            let data = line_rx.recv().expect("Failed to receive data string.");
             
-            match event {
-                DebouncedEvent::NoticeWrite(p) => {
-                    if p == path {
-                        reader = consume_reader(reader, &line_tx).unwrap();
+            match verify_checksum(crc_string, data) {
+                Ok(s) => {
+                    match websocket_client.send_message(&OwnedMessage::Text(s)) {
+                        Ok(_) => (),
+                        Err(_) => break,
                     }
-                },
-                DebouncedEvent::NoticeRemove(p) => {
-                    if p == path {
-                        break;
-                    }
-                },
-                DebouncedEvent::Create(_) => (),
-                DebouncedEvent::Write(p) => {
-                    if p == path {
-                        reader = consume_reader(reader, &line_tx).unwrap();
-                    }
-                },
-                DebouncedEvent::Chmod(_) => (),
-                DebouncedEvent::Remove(p) => {
-                    if p == path {
-                        break;
-                    }
-                },
-                DebouncedEvent::Rename(p_old, _) => {
-                    if p_old == path {
-                        break;
-                    }
-                },
-                DebouncedEvent::Rescan => (),
-                DebouncedEvent::Error(_, _) => break,
+                }
+                Err(_) => println!("Failed checksum"), //OK to continue
             }
         }
-        
-        receiver_thread.join().unwrap();
+    });
+
+    receiver_handle
+}
+
+fn main() {
+    //Read in arguments
+    let default_data_file_path = "/sdcard/Android/data/com.mersive.solstice.server/files/Logs/server.json";
+    let default_websocket_address = "ws://192.168.2.19:8443/produce";
+    
+    let mut args = env::args();
+    let _ = args.next();
+
+    //Get data file path
+    let data_file_path = args.next().unwrap_or(String::from(default_data_file_path));
+
+    let path = Path::new(&data_file_path);
+    if !path.exists() || !path.is_file() {
+        panic!("Data file passed in does not exist.")
     }
+
+    let parent = path.parent().expect("File passed in has no parent directory.");
+
+    //Get websocket address
+    let websocket_address = args.next().unwrap_or(String::from(default_websocket_address));
+
+    //Start reading from data file
+    let (mut reader, line_tx, line_rx) = tail_file(path.to_path_buf())
+        .expect("Failed to start tailing file.");
+
+    //Start watching for file system changes
+    let (watcher, rx) = start_watching(parent.to_path_buf())
+        .expect("Failed to start watching file system.");
+    
+    //Create weboscket and connect to server
+    //Spawn thread to pump data file data
+    let receiver_handle = start_websocket_thread(line_rx, websocket_address);
+    
+    //Process file system events
+    loop {
+        let event = rx.recv().unwrap();
+        
+        match event {
+            DebouncedEvent::NoticeWrite(p) => {
+                if p == path {
+                    reader = consume_reader(reader, &line_tx)
+                        .expect("Failed to read updated data.");
+                }
+            },
+            DebouncedEvent::NoticeRemove(p) => {
+                if p == path {
+                    break;
+                }
+            },
+            DebouncedEvent::Create(_) => (),
+            DebouncedEvent::Write(p) => {
+                if p == path {
+                    reader = consume_reader(reader, &line_tx)
+                        .expect("Failed to read updated data.");
+                }
+            },
+            DebouncedEvent::Chmod(_) => (),
+            DebouncedEvent::Remove(p) => {
+                if p == path {
+                    break;
+                }
+            },
+            DebouncedEvent::Rename(p_old, _) => {
+                if p_old == path {
+                    break;
+                }
+            },
+            DebouncedEvent::Rescan => (),
+            DebouncedEvent::Error(_, _) => break,
+        }
+    }
+    
+    receiver_handle.join().unwrap();
 }
